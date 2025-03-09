454a380a789660b12edcff2e4b32ef9320b79a02
Pritom Sarkar <pritom.cse.29@gmail.com>
2025-03-09 12:04:06 +0600

    Media files problem solved

--- event_management/urls.py
+++ event_management/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('no-permission/',no_permission,name='no-permission')
 ]+ debug_toolbar_urls()
 
-urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+ 
